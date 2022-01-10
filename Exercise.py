dfaee6767109638a370d749ea146be60c7a25cdc
nkwentibrice <anuaforbrice@gmail.com>
2022-01-09 21:41:27 -0800

    All_files

--- Exercise.py
+++ Exercise.py
@@ -21,7 +21,7 @@ for range in column:
         updated_email = printable.replace('helpinghands.cm', 'handsinhands.org')
         output.value = updated_email
 
-#save file that has been edited in new excel and csv sheet
+#save file that has been edited in new excel and csv sheet respectively
 wb_obj.save(filename="employeeupdateddata.xlsx")
 wb_obj.save(filename="employeeupdateddata.csv")
 
